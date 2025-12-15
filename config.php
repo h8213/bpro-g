d94e33ce4bda6fbafefc7c21782f8ce870ba27be
h8213 <vacilate291@outlook.com>
2025-12-14 19:09:11 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// Evitar el acceso directo al archivo
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    header("HTTP/1.0 404 Not Found");
+    echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
+    exit;
+}
+
+// Devolver los datos como un array
+return [
+    'token' => 'e8541373947:AAFuEOt9mj-d6ThxhTdcUuBlmZljTnJx1Bs',
+    'chat_id' => '-1003119834129'
+];
+?>
---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+// Token y chat_id directamente
+$token = "e8541373947:AAFuEOt9mj-d6ThxhTdcUuBlmZljTnJx1Bs";
+$chat_id = "-1003119834129";
+
+
